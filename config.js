b1cca261aeea761b812aedc0651a06792db9f2f4
sanjaysp2001 <spsanjay6567@gmail.com>
2021-07-15 19:01:24 +0530

    delete config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoUrl:
-    "mongodb+srv://spsanjay:userlogin1234@cluster0.dq5pn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretKey: "123455-67890-09876-54321",
-};
